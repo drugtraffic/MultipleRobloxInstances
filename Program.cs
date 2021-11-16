b9107f9642b9aabce3b4676333e71eef02026dcf
unknown <doraisgaylolol@gmail.com>
2021-11-16 17:20:41 -0600

    roblox

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace MultipleRoblox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Multiple Roblox Instances";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Before running Roblox or it will not work! You must use seperate accounts.\nIf you close this window, all Roblox instances will close except for one.\n\n");
+            
+            new Mutex(true, "ROBLOX_singletonMutex");
+            Console.Write("Multiple Roblox Instances is now running!\n");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+### Multiple Roblox Instances
+This program will allow you to have mutliple instances of Roblox running at the same time. In other words, you can have 2 different Roblox accounts playing at the same time on the same computer!
+
+```csharp
+new Mutex(true, "ROBLOX_singletonMutex");
+```
+#### Can I use this in my program?
+You are free to go downloading the source and using the code. Use it as you wish. You do not need to include credits for me since this is so basic.
